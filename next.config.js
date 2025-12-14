efdcba767a154888f91c65d3fae0b7094dd0cf82
sunjieseu <jie.sun@seu.edu.cn>
2025-12-14 18:37:25 +0800

    Update personal style v6

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  // 个人主页不需要 basePath，因为它部署在根域名
+  // 确保静态资源使用相对路径
+  assetPrefix: '',
+  // 个人主页部署在根域名，不需要 basePath
 }
 
 module.exports = nextConfig
